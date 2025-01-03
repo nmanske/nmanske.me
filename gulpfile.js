f2caa05156d0e587272dd1f5fa50b0c4a3d51bc3
Nathan Manske <nathan.manske@utexas.edu>
2025-01-03 15:42:46 -0600

    Clean up gulpfile and fix an issue where browser wouldn't refresh when saving .scss

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,8 @@ const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
 const sass = require('gulp-sass')(require('sass'));
 const sourcemaps = require('gulp-sourcemaps');
-const rename = require('gulp-rename');
-const uglify = require('gulp-uglify');
-const cssnano = require('gulp-cssnano');
-const imagemin = require('gulp-imagemin');
-const del = require('del');
 
-
+// BrowserSync for Development
 gulp.task('browserSyncDev', done => {
   browserSync.init({
     server: './',
@@ -16,67 +11,18 @@ gulp.task('browserSyncDev', done => {
   done();
 });
 
-gulp.task('sassDev', () => {
+// Compile SCSS
+gulp.task('sass', () => {
   return gulp.src('assets/styles/scss/main.scss')
-    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-    // .pipe(rename({ suffix: '.min' })) // TODO: Uncomment when compression works
+    .pipe(sourcemaps.init())
+    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+    .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('assets/styles/css'))
     .pipe(browserSync.stream());
 });
 
-gulp.task('html', () => {
-  return gulp.src('index.html')
-    .pipe(gulp.dest('dist'))
-})
-
-gulp.task('sass', () => {
-  return gulp.src('assets/styles/scss/main.scss')
-    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-    // .pipe(rename({ suffix: '.min' })) // TODO: Uncomment when compression works
-    .pipe(gulp.dest('dist/styles/css'))
-});
-
-gulp.task('sassMap', () => {
-  return gulp.src('assets/styles/scss/main.scss')
-    .pipe(sourcemaps.init())
-    .pipe(sass())
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('dist/styles/css'))
-});
-
-gulp.task('js', () => {
-  return gulp.src('assets/scripts/**/*.js')
-    .pipe(gulp.dest('dist/scripts'))
-    .pipe(rename('scripts.min.js'))
-    .pipe(uglify())
-    .pipe(gulp.dest('dist/scripts'));
-});
-
-gulp.task('images', () => {
-  return gulp.src('assets/images/*.+(png|jpg|jpeg|gif|svg)')
-  .pipe(cache(imagemin({
-    interlaced: true,
-  })))
-  .pipe(gulp.dest('dist/images'))
-});
-
-gulp.task('doc', () => {
-  return gulp.src('assets/doc/*.pdf')
-    .pipe(gulp.dest('dist/doc'))
-});
-
-gulp.task('clean:dist', () => del.sync('dist'));
-
-gulp.task('cache:clear', callback => cache.clearAll(callback));
-
-
-// Run for file watch fix:
-// echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
-gulp.task('default', gulp.series('browserSyncDev', 'sassDev', () => {
+// Watch Files for Changes
+gulp.task('default', gulp.series('browserSyncDev', 'sass', () => {
   gulp.watch('assets/styles/scss/*.scss', gulp.series('sass'));
   gulp.watch(['*.html', '**/*.js']).on('change', browserSync.reload);
 }));
-
-// gulp.task('build', gulp.parallel(gulp.series('clean', 'html', 'sass', 'sassMap', 'js', 'images', 'doc'), function (){
-//   console.log('Building files');
-// }));
